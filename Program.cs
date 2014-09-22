1ea4828d5a376b999a5de6695ae25b6f4ad389c6
limeng <limengabc@163.com>
2014-09-22 21:35:13 +0800

    get section regex items

--- Program.cs
+++ Program.cs
@@ -15,10 +15,23 @@ namespace regex_analyze_demo
 			// parser mapping file
 			Dictionary<string, IEnumerable<string>> mapping = MappingFile.getMappingDictionary();
 			
+			//parser regex files
 			Dictionary<string, IEnumerable<RegexItem>> regexDictionary = RegexFiles.getRegexDictionary();
+			
+			IEnumerable<RegexItem> sectionRegexItems = getRegexItems("zh","section","content");
 			
 			Console.Write("Press any key to continue . . . ");
 			Console.ReadKey(true);
+		}
+		
+		public static IEnumerable<RegexItem> getRegexItems(string language, string folder, string file)
+		{
+			string key = string.Format("{0}_{1}_{2}", language, folder, file).Trim('_');
+			if (RegexFiles.getRegexDictionary().ContainsKey(key))
+			{
+				return RegexFiles.getRegexDictionary()[key];
+			}
+			return new List<RegexItem>();
 		}
 	}
 }
